68641dc728442b617143112b83c431600390d628
Amrit Dash <amrit.dash60@gmail.com>
2020-04-04 22:13:42 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@ const catalogue_div = document.querySelector("#catalogue_div");
 db.collection("books").get().then(function(querySnapshot) {
   console.log("right");
   querySnapshot.forEach(function(doc) {
+    console.log(doc.data().name);
       catalogue_div.innerHTML = "<div class='flip-card'>" +
       "<div class='flip-card-inner'>" +
       "<div class='flip-card-front'>" +
@@ -13,34 +14,8 @@ db.collection("books").get().then(function(querySnapshot) {
       "</div>" +
       "<div class='flip-card-back'>" +
       "<h5>" + doc.data().name + "</h5>" +
-      // "<p>" + doc.data().description + "</p>" +
       "</div>" +
       "</div>" +
       "</div>"
   });
 });
-
-// db.collection("books").get().then(function(querySnapshot) {
-//     console.log("right");
-//     querySnapshot.forEach(function(doc) {
-//         catalogue_div.innerHTML += "<article>" +
-//         "<img src=" + doc.data().cover + " alt=" + doc.data().name + ">" + 
-//         "<div class='text'>" +
-//           "<h6>" + doc.data().name + "</h6>" +
-//         "</div>" +
-//       "</article>"
-//     });
-// });
-
-
-// db.collection("cities").where("state", "==", "CA")
-//   .onSnapshot({ includeMetadataChanges: true }, function(snapshot) {
-//       snapshot.docChanges().forEach(function(change) {
-//           if (change.type === "added") {
-//               console.log("New city: ", change.doc.data());
-//           }
-
-//           var source = snapshot.metadata.fromCache ? "local cache" : "server";
-//           console.log("Data came from " + source);
-//       });
-//   });
