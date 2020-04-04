e8f973eca94c383b84083b89662a031d3a3b2f5a
Amrit Dash <amrit.dash60@gmail.com>
2020-04-04 22:38:13 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ db.collection("books").get().then(function(querySnapshot) {
   console.log("right");
   querySnapshot.forEach(function(doc) {
     console.log(doc.data().name);
-      catalogue_div.innerHTML = "<div class='flip-card'>" +
+      catalogue_div.innerHTML += "<div class='flip-card'>" +
       "<div class='flip-card-inner'>" +
       "<div class='flip-card-front'>" +
       "<img src=" + doc.data().cover + " alt=" + doc.data().name + ">" +
